7ae8b5ce200731dc714113c7f7a45844a71ecf4b
Siri Ganapathineedi <snganapat@gmail.com>
2023-08-01 12:41:21 -0500

    Initial Commit

---
+++ ToDoListDemo/ToDoListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListDemoApp.swift
+//  ToDoListDemo
+//
+//  Created by Siri Ganapathineedi on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
